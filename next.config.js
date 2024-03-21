7730e9f72f7a92a2444cbc6bc5159be80fb40ae6
Hallex Abreu <hallexabreu@Hallexs-MacBook-Air.local>
2024-03-21 18:58:17 -0300

    fix: correction

--- next.config.js
+++ next.config.js
@@ -1,14 +1,11 @@
-import { default as withPWA } from "@ducanh2912/next-pwa";
-import { NextConfig } from 'next';
-
-const pwaOptions: any = {
+const withPWA = require("@ducanh2912/next-pwa").default({
   cacheOnFrontEndNav: true,
   aggressiveFrontEndNavCaching: true,
   reloadOnOnline: true,
   swcMinify: true,
   dest: "public",
   fallbacks: {
-    // image: "/static/images/fallback.png",
+    //image: "/static/images/fallback.png",
     document: "/offline", // if you want to fallback to a custom page rather than /_offline
     // font: '/static/font/fallback.woff2',
     // audio: ...,
@@ -18,12 +15,10 @@ const pwaOptions: any = {
     disableDevLogs: true,
   },
   // ... other options you like
-};
-
-const nextConfig: NextConfig = {
+});
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   // ... other options you like
 };
 
-const withPWAConfig = withPWA(pwaOptions);
-
-export default withPWAConfig(nextConfig);
+module.exports = withPWA(nextConfig);
