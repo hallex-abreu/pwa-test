bdb8e40822f09d22e137d93b9db448553137e62e
Hallex Abreu <hallexabreu@Hallexs-MacBook-Air.local>
2024-03-21 19:14:34 -0300

    fix

--- next.config.js
+++ next.config.js
@@ -1,11 +1,17 @@
-const runtimeCaching = require("next-pwa/cache");
-const withPWA = require('next-pwa')({
- dest: 'public', 
- register: true,
- skipWaiting: true,
- runtimeCaching
-})
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV !== "development",
+  },
+};
 
-module.exports = withPWA({
- reactStrictMode: false
-})
+const withPWA = require("next-pwa")({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+  skipWaiting: true,
+});
+
+module.exports = withPWA(nextConfig);
--- src/app/offline/page.tsx
+++
@@ -1,6 +0,0 @@
-export default function Offline() {
-    return (
-      <h1>TESTE PWA OFFLINE</h1>
-    );
-  }
-  
